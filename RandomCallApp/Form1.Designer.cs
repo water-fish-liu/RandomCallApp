0008a36a91981ef5e210e491043b40b1721942f1
lwen <2769317703@qq.com>
2024-03-08 22:20:28 +0800

    添加项目文件。

---
+++ RandomCallApp/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace RandomCallApp
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRandomCall = new System.Windows.Forms.Button();
+            this.buttonChooseFile = new System.Windows.Forms.Button();
+            this.buttonPreviewStudentList = new System.Windows.Forms.Button();
+            this.buttonViewRecentlyCalled = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonRandomCall
+            // 
+            this.buttonRandomCall.Font = new System.Drawing.Font("黑体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonRandomCall.Location = new System.Drawing.Point(12, 61);
+            this.buttonRandomCall.Name = "buttonRandomCall";
+            this.buttonRandomCall.Size = new System.Drawing.Size(260, 55);
+            this.buttonRandomCall.TabIndex = 0;
+            this.buttonRandomCall.Text = "点击进行随机点名";
+            this.buttonRandomCall.UseVisualStyleBackColor = true;
+            this.buttonRandomCall.Click += new System.EventHandler(this.buttonRandomCall_Click_1);
+            // 
+            // buttonChooseFile
+            // 
+            this.buttonChooseFile.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonChooseFile.Location = new System.Drawing.Point(12, 190);
+            this.buttonChooseFile.Name = "buttonChooseFile";
+            this.buttonChooseFile.Size = new System.Drawing.Size(260, 46);
+            this.buttonChooseFile.TabIndex = 1;
+            this.buttonChooseFile.Text = "选择学生名单";
+            this.buttonChooseFile.UseVisualStyleBackColor = true;
+            this.buttonChooseFile.Click += new System.EventHandler(this.buttonChooseFile_Click_1);
+            // 
+            // buttonPreviewStudentList
+            // 
+            this.buttonPreviewStudentList.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonPreviewStudentList.Location = new System.Drawing.Point(12, 131);
+            this.buttonPreviewStudentList.Name = "buttonPreviewStudentList";
+            this.buttonPreviewStudentList.Size = new System.Drawing.Size(125, 44);
+            this.buttonPreviewStudentList.TabIndex = 2;
+            this.buttonPreviewStudentList.Text = "预览";
+            this.buttonPreviewStudentList.UseVisualStyleBackColor = true;
+            this.buttonPreviewStudentList.Click += new System.EventHandler(this.buttonPreviewStudentList_Click_1);
+            // 
+            // buttonViewRecentlyCalled
+            // 
+            this.buttonViewRecentlyCalled.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonViewRecentlyCalled.Location = new System.Drawing.Point(155, 131);
+            this.buttonViewRecentlyCalled.Name = "buttonViewRecentlyCalled";
+            this.buttonViewRecentlyCalled.Size = new System.Drawing.Size(117, 44);
+            this.buttonViewRecentlyCalled.TabIndex = 3;
+            this.buttonViewRecentlyCalled.Text = "历史";
+            this.buttonViewRecentlyCalled.UseVisualStyleBackColor = true;
+            this.buttonViewRecentlyCalled.Click += new System.EventHandler(this.buttonViewRecentlyCalled_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(97, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "点名器";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "powered by C#,made by lwen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "@2023";
+            // 
+            // Form
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonViewRecentlyCalled);
+            this.Controls.Add(this.buttonPreviewStudentList);
+            this.Controls.Add(this.buttonChooseFile);
+            this.Controls.Add(this.buttonRandomCall);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "随机点名";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.FormClosing += Form_Closing;
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonRandomCall;
+        private System.Windows.Forms.Button buttonChooseFile;
+        private System.Windows.Forms.Button buttonPreviewStudentList;
+        private System.Windows.Forms.Button buttonViewRecentlyCalled;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RandomCallApp/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using ExcelDataReader;
+
+namespace RandomCallApp
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private List<string> recentlyCalledStudents = new List<string>();
+        private List<string> studentList = new List<string>();
+
+        public Form()
+        {
+            InitializeComponent();
+            LoadStudentList();
+            LoadRecentlyCalledStudents();
+        }
+
+        private void LoadStudentList()
+        {
+            try
+            {
+                using (StreamReader file = File.OpenText("student_list.json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    studentList = (List<string>)serializer.Deserialize(file, typeof(List<string>));
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                studentList = new List<string>();
+            }
+        }
+
+        private void SaveStudentList()
+        {
+            using (StreamWriter file = File.CreateText("student_list.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, studentList);
+            }
+        }
+
+        private List<string> ChooseFileDirectory()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx";
+            openFileDialog.Title = "选择学生名单文件";
+
+            List<string> newStudentList = new List<string>(); // 创建一个新的列表来保存新的学生数据
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
+                {
+                    using (var reader = ExcelReaderFactory.CreateReader(stream))
+                    {
+                        while (reader.Read())
+                        {
+                            newStudentList.Add(reader.GetString(0)); // 将新的学生数据添加到新的列表中
+                        }
+                    }
+                }
+
+                return newStudentList; // 返回新的学生数据列表
+            }
+            else
+            {
+                MessageBox.Show("未选择学生名单文件！", "错误");
+                return new List<string>(); // 返回空的列表
+            }
+        }
+
+        private void RandomCall(List<string> students)
+        {
+            if (students.Count > 0)
+            {
+                var availableStudents = students.Except(recentlyCalledStudents).ToList();
+                if (availableStudents.Count == 0)
+                {
+                    MessageBox.Show("所有同学都已被点名！", "随机点名");
+                    return;
+                }
+
+                Random random = new Random();
+                string randomStudent = availableStudents[random.Next(availableStudents.Count)];
+                recentlyCalledStudents.Add(randomStudent);
+                if (recentlyCalledStudents.Count > 5)
+                {
+                    recentlyCalledStudents.RemoveAt(0);
+                }
+                SaveRecentlyCalledStudents();
+                MessageBox.Show($"点名结果：{randomStudent}", "随机点名");
+            }
+            else
+            {
+                MessageBox.Show("学生名单为空！", "随机点名");
+            }
+        }
+
+        private void LoadRecentlyCalledStudents()
+        {
+            try
+            {
+                using (StreamReader file = File.OpenText("recently_called_students.json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    recentlyCalledStudents = (List<string>)serializer.Deserialize(file, typeof(List<string>));
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                recentlyCalledStudents = new List<string>();
+            }
+        }
+
+        private void SaveRecentlyCalledStudents()
+        {
+            using (StreamWriter file = File.CreateText("recently_called_students.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, recentlyCalledStudents);
+            }
+        }
+
+        private void PreviewStudentList(List<string> students)
+        {
+            if (students.Count > 0)
+            {
+                string studentListStr = string.Join("\n", students);
+                int numStudents = students.Count;
+                MessageBox.Show($"总人数：{numStudents}\n学生名单预览：\n{studentListStr}\n", "学生名单预览");
+            }
+            else
+            {
+                MessageBox.Show("学生名单为空！", "学生名单预览");
+            }
+        }
+
+        
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonRandomCall_Click_1(object sender, EventArgs e)
+        {
+            RandomCall(studentList);
+        }
+
+        private void buttonChooseFile_Click_1(object sender, EventArgs e)
+        {
+            studentList = ChooseFileDirectory();
+
+        }
+
+        private void buttonPreviewStudentList_Click_1(object sender, EventArgs e)
+        {
+            PreviewStudentList(studentList);
+        }
+
+        private void buttonViewRecentlyCalled_Click_1(object sender, EventArgs e)
+        {
+            if (recentlyCalledStudents.Any())
+            {
+                string recentlyCalledStr = string.Join("\n", recentlyCalledStudents);
+                MessageBox.Show($"最近被点名学生：\n{recentlyCalledStr}", "最近被点名学生");
+            }
+            else
+            {
+                MessageBox.Show("还没有点名记录！", "最近被点名学生");
+            }
+        }
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            SaveStudentList();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
+
